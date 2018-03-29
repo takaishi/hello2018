1b953db5b1be09bfa93e569ccdfa2611b7026e4e
r_takaishi <ryo.takaishi.0@gmail.com>
2018-03-29 21:06:26 +0900

    init

---
+++ grpc_with_transport_credentials/README.md
@@ -0,0 +1,6 @@
+# grpc.WithTransportCredentials
+
+- [grpc-goのInterceptorを使ってみる](https://qiita.com/Mamoru-Izuka/items/28724d9dd8a6b30b236d)
+- https://mattn.kaoriya.net/software/lang/go/20150227144125.htm
+- https://github.com/grpc/grpc-go/issues/106#issuecomment-246978683
+- https://github.com/buckhx/safari-zone
---
+++ grpc_with_transport_credentials/server/server.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"golang.org/x/net/context"
+	"log"
+	"net"
+	"sync"
+
+	"google.golang.org/grpc"
+
+	"fmt"
+	pb "github.com/takaishi/hello2018/grpc_password_auth/protocol"
+	"github.com/takaishi/hello2018/grpc_password_auth/server/auth"
+)
+
+type customerService struct {
+	customers []*pb.Person
+	m         sync.Mutex
+}
+
+func (cs *customerService) ListPerson(p *pb.RequestType, stream pb.CustomerService_ListPersonServer) error {
+	fmt.Println("ListPerson")
+	cs.m.Lock()
+	defer cs.m.Unlock()
+	for _, p := range cs.customers {
+		if err := stream.Send(p); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (cs *customerService) AddPerson(c context.Context, p *pb.Person) (*pb.ResponseType, error) {
+	fmt.Println("AddPerson")
+	cs.m.Lock()
+	defer cs.m.Unlock()
+	cs.customers = append(cs.customers, p)
+	return new(pb.ResponseType), nil
+}
+
+func main() {
+	a := auth.NewAuthorizer("admin", "admin123")
+
+	lis, err := net.Listen("tcp", ":11111")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	//stm := []grpc.StreamServerInterceptor{}
+
+	server := grpc.NewServer(
+		grpc.StreamInterceptor(a.HandleStream),
+		grpc.UnaryInterceptor(a.HandleUnary),
+	)
+
+	fmt.Printf("server: %#v\n", server)
+
+	pb.RegisterCustomerServiceServer(server, new(customerService))
+	server.Serve(lis)
+}
