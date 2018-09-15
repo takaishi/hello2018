4b010c64b8f7fefdc9370e44b410ac4aa672f4b8
r_takaishi <ryo.takaishi.0@gmail.com>
2018-09-15 09:25:31 +0900

    write unit test

--- learn-rust/euclidean_algorithm/src/main.rs
+++ learn-rust/euclidean_algorithm/src/main.rs
@@ -14,3 +14,12 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
   }
   n
 }
+
+#[test]
+fn test_gcd() {
+  assert_eq!(gcd(14, 15), 1);
+
+  assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
+                 3 * 7 * 11 * 13 * 19),
+             3 * 11);
+}
