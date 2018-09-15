c41d417d87ac92e2761a7ffb302a1f6198107770
r_takaishi <ryo.takaishi.0@gmail.com>
2018-09-15 09:47:37 +0900

    add gcd server

--- learn-rust/iron-gcd/src/main.rs
+++ learn-rust/iron-gcd/src/main.rs
@@ -1,3 +1,27 @@
+extern crate iron;
+#[macro_use] extern crate mime;
+
+use iron::prelude::*;
+use iron::status;
+
 fn main() {
-    println!("Hello, world!");
+  println!("Serving on http://localhost:3000...");
+  Iron::new(get_form).http("localhost:3000").unwrap();
+}
+
+fn get_form(_request: &mut Request) -> IronResult<Response> {
+  let mut response = Response::new();
+
+  response.set_mut(status::Ok);
+  response.set_mut(mime!(Text/Html; Charset=Utf8));
+  response.set_mut(r#"
+    <title>GCD Calcularor</title>
+    <form action="/gcd" method="post">
+      <input type="text" name="n"/>
+      <input type="text" name="m"/>
+      <button type="submit">Compute GCD</button>
+    </form>
+  "#);
+
+  Ok(response)
 }
