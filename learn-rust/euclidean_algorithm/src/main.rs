c3e0fd7423a6abeeeb24d523871ffbef5daa2289
r_takaishi <ryo.takaishi.0@gmail.com>
2018-09-15 09:47:18 +0900

    update to cli with argument

--- learn-rust/euclidean_algorithm/src/main.rs
+++ learn-rust/euclidean_algorithm/src/main.rs
@@ -1,5 +1,25 @@
+use std::io::Write;
+use std::str::FromStr;
+
 fn main() {
-  println!("{}", format!("{}", gcd(10, 2)));
+  let mut numbers = Vec::new();
+
+  for arg in std::env::args().skip(1) {
+    numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+  }
+
+  if numbers.len() == 0 {
+    writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+    std::process::exit(1);
+  }
+
+  let mut d = numbers[0];
+  for m in &numbers[1..] {
+    d = gcd(d, *m);
+  }
+
+  println!("The greatest common divisor of {:?} is {}", numbers, d);
+
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
