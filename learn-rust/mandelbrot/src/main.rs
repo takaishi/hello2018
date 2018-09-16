ce41af5d382ddb0e137d162116c7ba34c588a49b
r_takaishi <ryo.takaishi.0@gmail.com>
2018-09-16 21:13:13 +0900

    impl pixel_to_point

--- learn-rust/mandelbrot/src/main.rs
+++ learn-rust/mandelbrot/src/main.rs
@@ -38,6 +38,20 @@ fn parse_complex(s: &str) -> Option<Complex<f64>> {
     }
 }
 
+fn pixel_to_point(bounds: (usize, usize),
+                  pixel: (usize, usize),
+                  upper_left: Complex<f64>,
+                  lower_right: Complex<f64>)
+    -> Complex<f64> {
+    let (width, height) = (lower_right.re - upper_left.re,
+                           upper_left.im - lower_right.im);
+
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+    }
+
+}
 #[test]
 fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>("", ','), None);
@@ -53,4 +67,13 @@ fn test_parse_pair() {
 fn test_parse_complex() {
     assert_eq!(parse_complex("1.25,-0.0625"), Some(Complex { re: 1.25, im: -0.0625 }));
     assert_eq!(parse_complex(",-0.0625"), None)
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100, 100),
+                              (25, 75),
+                              Complex { re: -1.0, im: 1.0},
+                              Complex { re: 1.0, im: -1.0}),
+               Complex { re: -0.5, im: -0.5 });
 }
