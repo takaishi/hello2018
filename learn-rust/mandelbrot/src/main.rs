f1b7b833336cc718becc7fba191fdaf52798e49d
r_takaishi <ryo.takaishi.0@gmail.com>
2018-09-25 21:39:34 +0900

    impl serial main

--- learn-rust/mandelbrot/src/main.rs
+++ learn-rust/mandelbrot/src/main.rs
@@ -7,9 +7,27 @@ use num::Complex;
 use image::ColorType;
 use image::png::PNGEncoder;
 use std::fs::File;
+use std::io::Write;
 
 fn main() {
     println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 5 {
+        writeln!(std::io::stderr(), "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT").unwrap();
+        writeln!(std::io::stderr(), "Example: {} mandel.png 1000x750 -1.25,0.35 -1,0.20", args[0]).unwrap();
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimentions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
 
 fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
