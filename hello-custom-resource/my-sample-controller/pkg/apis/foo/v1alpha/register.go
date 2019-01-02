b48590a5fc3868a062a043e7e976c4d71837b884
r_takaishi <ryo.takaishi.0@gmail.com>
2019-01-02 12:48:30 +0900

    Foo

--- hello-custom-resource/my-sample-controller/pkg/apis/foo/v1alpha/register.go
+++ hello-custom-resource/my-sample-controller/pkg/apis/foo/v1alpha/register.go
@@ -29,8 +29,8 @@ var (
 // Adds the list of known types to Scheme.
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
-		&Workflow{},
-		&WorkflowList{},
+		&Foo{},
+		&FooList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
